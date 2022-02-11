f50f3c558798e93a5b43bf37140b52baa6a976c7
umikami <ggoku02@gmail.com>
2022-02-10 23:36:58 -0500

    Book grid

--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
-import ReactDom from 'react-dom'
+import ReactDom from "react-dom";
+import './style.css'
 
-function Greeting(){
-  return <h1>Hello, this my first React component</h1>
+function BookList() {
+  return (
+    <section className="booklist">
+      <Book/>
+      <Book/>
+      <Book/>
+      <Book/>
+      <Book/>
+    </section>
+  )
 }
 
+const Book = () => {
+  return (
+    <article className="book">
+      <Image></Image>
+      <Title/>
+      <Author/>
+    </article>
+  )
+}
 
-ReactDom.render(<Greeting/>, document.getElementById('root'))
+const Image = () => <img src="https://images-na.ssl-images-amazon.com/images/I/71aG+xDKSYL._AC_UL604_SR604,400_.jpg" alt="book cover" />
+
+const Title = () => <h1>The 48 Laws of Power</h1>
+const Author = () => <h1>Robert Greene</h1>
+
+ReactDom.render(<BookList />, document.getElementById("root"));
